088298284a14adbf08024faa6e0a95a218095920
Gospik <burakkarabacak2002@gmail.com>
2024-05-19 18:35:23 +0300

    Added 1 new database list

---
+++ MProject/Pages/DataTransactions/GlobalClass.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MProject.Pages.DataTransactions
+{
+   public class GlobalClass
+    {
+        public int ID {  get; set; }
+        public string Name { get; set; } 
+        public string Course_Code {  get; set; }
+
+        public GlobalClass() {  }
+
+        public GlobalClass(int id,string name,string course_code) 
+        {
+            ID = id;
+            Name= name;
+            Course_Code = course_code;
+
+        }
+    }
+}
